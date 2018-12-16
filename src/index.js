e340c1b6e45c5db8b3e88653f57fd024e1784748
Brandon Schabel <brandonschabel1995@gmail.com>
2018-12-16 15:37:57 -0800

    setup apolloclient

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import 'dotenv/config';
 import ApolloClient from 'apollo-boost';
+import 'cross-fetch/polyfill';
 
 const client = new ApolloClient ({
   uri: 'https://api.github.com/graphql',
   request: operation => {
-    operation.setContext
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_TOKEN}`
+      }
+    })
   }
 })
 
