9d5df14075d3ac7455bf746430a959c994da2863
Brandon Schabel <brandonschabel1995@gmail.com>
2018-12-16 18:11:44 -0800

    setup apollo with react

--- react-graphql-github-apollo-starter-kit/src/index.js
+++ react-graphql-github-apollo-starter-kit/src/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
+import { ApolloClient } from "apollo-client"
+import { HttpLink } from "apollo-link-http"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { ApolloProvider } from "react-apollo" // what actually connects react to apollo
 
-import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import registerServiceWorker from "./registerServiceWorker"
+import App from "./App"
 
-import './style.css';
+import "./style.css"
+
+const GITHUB_BASE_URL = "https://api.github.com/graphql"
+const cache = new InMemoryCache() // the store
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`
+  }
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+})
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+)
 
-registerServiceWorker();
+registerServiceWorker()
